696201c2ee533ff01c202c6a54cba6b8c88bdfaf
adambenhassen <adambenhassen@hotmail.com>
2026-07-26 15:32:25 +0000

    test(api): widen chat fixture for create-action refs

--- internal/api/updates.go
+++ internal/api/updates.go
@@ -242,8 +242,6 @@ func (h *handlers) eventToUpdate(ctx context.Context, userID int64, ev store.Eve
 			switch m.Action {
 			case store.ChatActionAddUser, store.ChatActionDeleteUser:
 				refs = append(refs, m.ActionUserID)
-			case store.ChatActionCreate:
-				refs = append(refs, createUsers...)
 			}
 		} else {
 			refs = append(refs, m.PeerID)
